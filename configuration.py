ec64cbdb4abed547c62b7af3e5ecae9566a58d26
andiduh <diduh6@gmail.com>
2023-09-09 19:54:16 +0300

    Решений заданий финального спринта

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE ="https://8489df2d-1559-43a7-a98e-8db075077aaf.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders/"
+GET_ORDER = "/api/v1/orders/track/"
---
+++ get_order_information.py
@@ -0,0 +1,16 @@
+# Дидух Анастасия, 8-я когорта — Финальный проект. Инженер по тестированию плюс
+import configuration
+import requests
+import data
+# Создание заказа
+def post_new_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER, json=body)
+#Получение трека заказа
+response = post_new_order(data.user_body)
+track = str(response.json()["track"])
+# Запрос на получение заказа по треку заказа
+def get_order_track():
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER + "?t=" + track)
+#Проверка теста
+def test_get_order_information():
+    assert get_order_track().status_code == 200
